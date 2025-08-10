4f84d450041aa71808884f5cd3a4bed9c43313bb
Bolgoffour <Bolgoffour@gmail.com>
2025-08-10 23:58:19 +0300

    centimetre_to_meters

--- first_program.py
+++ first_program.py
@@ -1,2 +1,6 @@
-x = int(input())
-print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep='---')
+# x = int(input())
+# # print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep='---')
+
+centimetre =  265 #int(input())
+meters = centimetre // 100
+print(meters)

d5f7e38af15bbb24eb5a98b443a9c8af38cef098
Bolgoffour <Bolgoffour@gmail.com>
2025-08-22 17:49:12 +0300

    + Colors

--- first_program.py
+++ first_program.py
@@ -62,3 +62,14 @@ elif opr == '/':
 else:
     print('Неверная операция')
  
+ color1 = input()
+color2 = input()
+if color1 == 'красный' and color2 == 'синий' or color1 == 'синий' and color2 == 'красный':
+    print('фиолетовый')
+elif color1 == 'красный' and color2 == 'желтый' or color1 == 'желтый' and color2 == 'красный':
+    print('оранжевый')
+elif color1 == 'синий' and color2 == 'желтый' or color1 == 'желтый' and color2 == 'синий':
+    print('зеленый')
+синий
+желтый
+красный
